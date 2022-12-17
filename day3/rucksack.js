cd96b1677c26879eb0e4cbb3c74262312969f2c5
whejy <john.wheldale@outlook.com>
2022-12-17 17:50:16 +1030

    refactor code

--- day3/rucksack.js
+++ day3/rucksack.js
@@ -1,7 +1,9 @@
 const input = require('fs').readFileSync('input.txt', 'utf-8').split('\n')
 
+let duplicates = []
 let total = 0
 
+// Helper function that converts each duplicate item to a 'priority' number
 function getPriority(letter) {
   if (letter === letter.toUpperCase()) {
     total += letter.charCodeAt(0) - 38
@@ -10,14 +12,13 @@ function getPriority(letter) {
   }
 }
 
-let duplicates = []
-
+// Get duplicate items in each rucksack
 for (let i = 0; i < input.length; i++) {
   let first = input[i].substring(0, input[i].length / 2).split('')
   let second = input[i].substring(input[i].length / 2).split('')
 
-  let set = new Set(first.filter((el) => second.includes(el)))
-  duplicates.push(Array.from(set))
+  let duplicate = first.find((el) => second.includes(el))
+  duplicates.push(duplicate)
 }
 
 duplicates.flat().forEach((dupe) => getPriority(dupe))
