3f03a93809cd34d9ec1bcdbb6c2ee20c23620061
whejy <john.wheldale@outlook.com>
2022-12-18 16:08:57 +1030

    day3 add part2

--- day3/rucksack.js
+++ day3/rucksack.js
@@ -1,26 +1,50 @@
 const input = require('fs').readFileSync('input.txt', 'utf-8').split('\n')
 
-let duplicates = []
-let total = 0
+let duplicates;
 
-// Helper function that converts each duplicate item to a 'priority' number
-function getPriority(letter) {
-  if (letter === letter.toUpperCase()) {
-    total += letter.charCodeAt(0) - 38
-  } else {
-    total += letter.charCodeAt(0) - 96
-  }
+// Sum items in rucksack according to priority value
+function getPriority(duplicates) {
+  return duplicates.reduce((acc, curr) => {
+    if (curr === curr.toUpperCase()) {
+      return acc + curr.charCodeAt(0) - 38
+    } else {
+      return acc + curr.charCodeAt(0) - 96
+    }
+  }, 0)
 }
 
 // Get duplicate items in each rucksack
-for (let i = 0; i < input.length; i++) {
-  let first = input[i].substring(0, input[i].length / 2).split('')
-  let second = input[i].substring(input[i].length / 2).split('')
+function getDuplicates() {
+  duplicates = [];
 
-  let duplicate = first.find((el) => second.includes(el))
-  duplicates.push(duplicate)
+  for (let i = 0; i < input.length; i++) {
+    let first = input[i].substring(0, input[i].length / 2).split('');
+    let second = input[i].substring(input[i].length / 2).split('');
+
+    let duplicate = first.find((el) => second.includes(el));
+    duplicates.push(duplicate);
+  }
+
+  return getPriority(duplicates)
 }
 
-duplicates.flat().forEach((dupe) => getPriority(dupe))
+// Get common item between 3 rucksacks
+function getBadges() {
+  duplicates = [];
 
-console.log(total)
+  for (let i = 0; i < input.length - 2; i += 3) {
+    let duplicate = input[i]
+      .split('')
+      .find(
+        (el) =>
+          input[i + 1].split('').includes(el) &&
+          input[i + 2].split('').includes(el)
+      );
+    duplicates.push(duplicate);
+  }
+
+  return getPriority(duplicates)
+}
+
+console.log(`Priority total for duplicate items: ${getDuplicates()}`);
+console.log(`Priority total for group badges: ${getBadges()}`);
