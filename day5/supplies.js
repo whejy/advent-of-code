1ecc8c502eaa9b682a503410c59dc043e59d914e
whejy <john.wheldale@outlook.com>
2022-12-20 16:03:37 +1030

    refactor code

--- day5/supplies.js
+++ day5/supplies.js
@@ -6,34 +6,42 @@ const moves = input[1].split('\n')
 const stackArr = [[], [], [], []]
 const movesArr = []
 
-for (let i = 0; i < stack.length; i++) {
-  // each column is 4 spaces wide
-  for (let j = 0; j < stack[i].length; j += 4) {
-    // push element to corresponding index
-    if (stack[i][j + 1] !== ' ' && isNaN(Number(stack[i][j + 1]))) {
-      stackArr[j / 4].push(stack[i][j + 1])
-      stackArr.push([])
+function getStacks() {
+  for (let i = 0; i < stack.length; i++) {
+    // each column is 4 spaces wide
+    for (let j = 0; j < stack[i].length; j += 4) {
+      // push element to corresponding index
+      if (stack[i][j + 1] !== ' ' && isNaN(Number(stack[i][j + 1]))) {
+        stackArr[j / 4].push(stack[i][j + 1])
+        stackArr.push([])
+      }
     }
   }
+
+  // remove empty arrays from stacks
+  const filteredStack = stackArr.filter((subArr) => subArr.length >= 1)
+  filteredStack.forEach((stack) => stack.reverse())
+  return filteredStack
 }
 
-// remove empty arrays from stacks
-const filteredStack = stackArr.filter((subArr) => subArr.length >= 1)
-filteredStack.forEach((stack) => stack.reverse())
+function moveStacks() {
+  const filteredStack = getStacks()
 
-// collect movements array
-moves.forEach((line) =>
-  movesArr.push(line.split(' ').filter((element) => !isNaN(element)))
-)
+  // collect movements array
+  moves.forEach((line) =>
+    movesArr.push(line.split(' ').filter((element) => !isNaN(element)))
+  )
 
-// perform crate movements
-movesArr.forEach((move) => {
-  let removedCrates = filteredStack[move[1] - 1].splice(-move[0])
-  filteredStack[move[2] - 1].push(...removedCrates.reverse())
-})
+  // perform crate movements
+  movesArr.forEach((move) => {
+    let removedCrates = filteredStack[move[1] - 1].splice(-move[0])
+    filteredStack[move[2] - 1].push(...removedCrates.reverse())
+  })
 
-// collect crates from top of each stack
-let topCrates = ''
-filteredStack.forEach((stack) => (topCrates += stack[stack.length - 1]))
+  // collect crates from top of each stack
+  let topCrates = ''
+  filteredStack.forEach((stack) => (topCrates += stack[stack.length - 1]))
+  return topCrates
+}
 
-console.log(topCrates)
+console.log(moveStacks())
